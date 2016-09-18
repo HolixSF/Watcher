5db1e30a402591b889402d51ceb919484950e9cd
Tedmund Chua <email": null>
2016-09-17 17:02:45 -0700

    Add map stuff

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  return $.ajax({
+  $.ajax({
     dataType: 'text',
     url: 'reports/tweets.json',
     success: function(data) {
@@ -9,4 +9,27 @@ $(document).ready(function() {
       return map.featureLayer.setGeoJSON(geojson);
     }
   });
+
+
+  centerMap();
+  popups();
 });
+
+
+var centerMap = function () {
+  map.featureLayer.on('click', function(e) {
+        map.panTo(e.layer.getLatLng());
+    });
+}
+var popups = function () {
+  map.featureLayer.on('layeradd', function(e) {
+    var marker, popupContent, properties;
+    marker = e.layer;
+    properties = marker.feature.properties;
+    popupContent = '<div class="popup">' + '<h3>' + properties.name + '</h3>' + '<p>' + properties.handle + '</p>' + '<h2>' + properties.content + '</h2>' + '</div>';
+    return marker.bindPopup(popupContent, {
+      closeButton: false,
+      minWidth: 320
+    });
+  });
+}
