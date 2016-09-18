4a3a0e2ef572ea0a32cc05e1bcf630342e12e809
Connor Ray <connor.ray.cb@gmail.com>
2016-09-18 10:06:41 -0700

    Adds loading animation for get report

--- app/assets/javascripts/anchor.js
+++ app/assets/javascripts/anchor.js
@@ -1,9 +1,16 @@
 $(document).on('ready', function(event) {
-	
+	$(".spinner").hide();
+
 	$( '#report_button' ).on('click', function() {
-		$(document.body).animate({
+		$(".g-report").hide();
+		$(".spinner").show();
+		setTimeout(function() { $(".spinner").hide(); }, 1300);
+		setTimeout(function() { $(".g-report").show(); }, 1300);
+
+		$(document.body).delay(1400).animate({
 	    'scrollTop':   $('#anchor1').offset().top
 		}, 1000);
+
 	});
 
 	$( '.report_icon' ).on('click', function() {
